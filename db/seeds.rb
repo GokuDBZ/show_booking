6e2eb783f21167cc867d90b81dcc4496ecd200c6
Suraj Yadav <suraj.yadav@modaoperandi.com>
2021-02-14 19:23:57 +0530

    removed sqlite and added seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,33 +7,33 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-   # movies = Movie.create([{name: 'Wonder Woman', genre: "Scifi"}, {name: 'Anabelle', genre: "Horror"}, {name: 'Tenet', genre: "Scifi"}, {name: "DDLJ", genre: "RomCom"}, {name: "Durgamati", genre: "Thriller"}, {name: "Avengers", genre: "Scifi"}])
+   movies = Movie.create([{name: 'Wonder Woman', genre: "Scifi"}, {name: 'Anabelle', genre: "Horror"}, {name: 'Tenet', genre: "Scifi"}, {name: "DDLJ", genre: "RomCom"}, {name: "Durgamati", genre: "Thriller"}, {name: "Avengers", genre: "Scifi"}])
 
-   # cinemas = Cinema.create([{name: "Cine Polis", address: "Logix Mall, City Center Noida"}, {name: "Wave Cinemas", address: "Wave Cinemas, Noida"}, {name: "PVR Cinema", address: "GIP Mall, Noida"}])
+   cinemas = Cinema.create([{name: "Cine Polis", address: "Logix Mall, City Center Noida"}, {name: "Wave Cinemas", address: "Wave Cinemas, Noida"}, {name: "PVR Cinema", address: "GIP Mall, Noida"}])
 
-   # screens = {}
-   # movies = Movie.all
-   # Cinema.all.to_a.each_with_index do |cinema, index|
-   # 	cin_name = cinema.name.split(" ").map{|n| n[0]}.join("")
-   # 	run = 1
-   # 	while(run <= 2) do
-   # 		if screens.has_key?(cinema.name)
-   # 		  screens[cinema.name] << Screen.create(name: cin_name + "-" + run.to_s , cinema: cinema)
-   # 	    else
-   # 		  screens[cinema.name] = [Screen.create(name: cin_name + "-" + run.to_s , cinema: cinema)]
-   # 	    end
-   # 	    run += 1 
-   # 	end
-   # end
+   screens = {}
+   movies = Movie.all
+   Cinema.all.to_a.each_with_index do |cinema, index|
+   	cin_name = cinema.name.split(" ").map{|n| n[0]}.join("")
+   	run = 1
+   	while(run <= 2) do
+   		if screens.has_key?(cinema.name)
+   		  screens[cinema.name] << Screen.create(name: cin_name + "-" + run.to_s , cinema: cinema)
+   	    else
+   		  screens[cinema.name] = [Screen.create(name: cin_name + "-" + run.to_s , cinema: cinema)]
+   	    end
+   	    run += 1 
+   	end
+   end
 
 
-   # Show.create(movie: Movie.first, cinema: Cinema.first, screen: Screen.first, timing: Show.timings.first)
-   # Show.create(movie: Movie.second, cinema: Cinema.first, screen: Screen.second, timing: 1)
+   Show.create(movie: Movie.first, cinema: Cinema.first, screen: Screen.first, timing: Show.timings.first)
+   Show.create(movie: Movie.second, cinema: Cinema.first, screen: Screen.second, timing: 1)
    
    movies = Movie.all.reverse
 
-   # Show.create(movie: movies.first, cinema: Cinema.where(name: "Wave Cinemas").first, screen: Cinema.where(name: "Wave Cinemas").first.screens[1], timing: 1)
-   # Show.create(movie: movies.first, cinema: Cinema.where(name: "PVR Cinema").first, screen: Cinema.where(name: "PVR Cinema").first.screens[1], timing: 0)
+   Show.create(movie: movies.first, cinema: Cinema.where(name: "Wave Cinemas").first, screen: Cinema.where(name: "Wave Cinemas").first.screens[1], timing: 1)
+   Show.create(movie: movies.first, cinema: Cinema.where(name: "PVR Cinema").first, screen: Cinema.where(name: "PVR Cinema").first.screens[1], timing: 0)
 
    Show.create(movie: movies.second, cinema: Cinema.where(name: "Wave Cinemas").first, screen: Cinema.where(name: "Wave Cinemas").first.screens[0], timing: 2)
    Show.create(movie: movies.second, cinema: Cinema.where(name: "Cine Polis").first, screen: Cinema.where(name: "Cine Polis").first.screens[1], timing: 1)
